0191ab14d148c428dccc871f30dc5869f5386b7b
Jesús <jesuslopj0@gmail.com>
2025-06-26 00:40:38 +0200

    Añadido  django.set_up() a asgi

--- techchain/asgi.py
+++ techchain/asgi.py
@@ -8,12 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns  # Rutas de WebSockets
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techchain.settings")
+django.set_up() # IMPORTANTE PARA CARGAR DJANGO EN ASWI
 
 application = ProtocolTypeRouter(
     {
