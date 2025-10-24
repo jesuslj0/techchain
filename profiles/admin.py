a7471f4f1cd35085cb80630c052fe561dd12feb2
Jesús <jesuslopj0@gmail.com>
2025-10-24 20:30:01 +0200

    Añadiendo usuario al admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import UserProfile, Follow
+from .models import User, UserProfile, Follow
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'uuid', 'username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser', 'date_joined']
+    
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
