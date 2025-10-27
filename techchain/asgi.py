6959639aaa9ae5e99bd0fc22fd41c0b697a8802c
Jesús <jesuslopj0@gmail.com>
2025-10-27 04:56:31 +0100

    Cambiando imports en asgi.py

--- techchain/asgi.py
+++ techchain/asgi.py
@@ -9,16 +9,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techchain.settings")
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns  # Rutas de WebSockets
 from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techchain.settings")
+
 application = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),  # Peticiones HTTP normales
+        "http": django_asgi_app,  # Peticiones HTTP normales
         "websocket": AuthMiddlewareStack(
             URLRouter(websocket_urlpatterns)  # Maneja WebSockets
         ),
